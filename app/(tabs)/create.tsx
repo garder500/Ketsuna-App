c2af9fd76add81e7c8ef87202f47f862fc403b38
exatombe <jeremy27.clara22@gmail.com>
2024-05-02 01:46:27 +0200

    Updating Disposale of the Notification handler (Need to improve some thing before releasing in Production)
    This should fix the Notification problem in development (To allow me to TEST FINALLY THAT SHIT)

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,49 +1,7 @@
 import * as Notifications from "expo-notifications";
-import * as Device from "expo-device";
 import { Button, Surface, Text } from 'react-native-paper';
-import { useEffect, useRef, useState } from "react";
-import { Platform } from "react-native";
-import Constants from "expo-constants";
-
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldPlaySound: false,
-        shouldSetBadge: true,
-        shouldShowAlert: true
-    })
-})
 
 export default function CreateScreen() {
-
-    const [expoPushToken, setExpoPushToken] = useState('');
-    const [notification, setNotification] = useState<Notifications.Notification>();
-    const notificationListener = useRef<Notifications.Subscription>()
-    const responseListener = useRef<Notifications.Subscription>();
-
-    useEffect(() => {
-        registerForPushNotificationsAsync().then(token => {
-            if (token) {
-                setExpoPushToken(token)
-                console.log(token);
-            }
-        });
-
-        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-            setNotification(notification);
-        });
-
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log(response);
-        });
-
-        return () => {
-            if (notificationListener.current && responseListener.current) {
-                Notifications.removeNotificationSubscription(notificationListener.current);
-                Notifications.removeNotificationSubscription(responseListener.current);
-            }
-        };
-    }, []);
-
 
     return (
         <Surface style={{
@@ -52,17 +10,11 @@ export default function CreateScreen() {
             justifyContent: 'center',
         }}>
             <Text style={{ textAlign: 'center' }} variant='titleLarge'>Create a new app</Text>
-            <Text>Your expo push token: {expoPushToken}</Text>
-            <Surface style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Title: {notification && notification.request.content.title} </Text>
-                <Text>Body: {notification && notification.request.content.body}</Text>
-                <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-            </Surface>
             <Button
                 onPress={async () => {
                     await schedulePushNotification();
                 }}
-            >Pres a Schedule notification</Button>
+            >Press a Schedule notification</Button>
         </Surface>
     );
 }
@@ -70,49 +22,28 @@ export default function CreateScreen() {
 
 
 async function schedulePushNotification() {
+    await Notifications.setNotificationCategoryAsync('test', [
+        {
+            identifier: 'textInput',
+            buttonTitle: 'Reply',
+            textInput: {
+                placeholder: 'Enter your response',
+                submitButtonTitle: 'Send',
+            }
+        }
+    ]);
     await Notifications.scheduleNotificationAsync({
         content: {
             title: "You've got mail! 📬",
             body: 'Here is the notification body',
-            data: { data: 'goes here' },
+            data: { data: 'test' },
+            badge: 1,
+            sticky: true,
+            subtitle: 'This is a subtitle',
+            launchImageName: 'icon.png',
+            color: '#FF231F7C',
+            categoryIdentifier: 'test',
         },
         trigger: { seconds: 2 },
     });
-}
-
-async function registerForPushNotificationsAsync() {
-    let token;
-
-    if (Platform.OS === 'android') {
-        await Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
-    }
-
-    if (Device.isDevice) {
-        const projectId = Constants.expoConfig?.extra ? Constants.expoConfig?.extra.eas.projectId : undefined;
-        if (projectId) {
-            console.log(projectId);
-            const { status: existingStatus } = await Notifications.getPermissionsAsync();
-            let finalStatus = existingStatus;
-            if (existingStatus !== 'granted') {
-                const { status } = await Notifications.requestPermissionsAsync();
-                finalStatus = status;
-            }
-            if (finalStatus !== 'granted') {
-                alert('Failed to get push token for push notification!');
-                return;
-            }
-            // Learn more about projectId:
-            // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-            token = (await Notifications.getExpoPushTokenAsync({ projectId })).data;
-            console.log(token);
-        } else {
-            alert('Must use physical device for Push Notifications');
-        }
-    }
-    return token;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,9 +8,13 @@ import {
   MD3LightTheme,
   adaptNavigationTheme,
 } from 'react-native-paper';
-import { Stack } from 'expo-router';
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import { useEffect, useRef, useState } from "react";
+import Constants from "expo-constants";
+import { Stack, router } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
-import { useColorScheme } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -39,20 +43,92 @@ const CombinedDarkTheme = {
   },
 };
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: true,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.MAX,
+          lightColor: '#FF231F7C',
+      });
+  }
+
+  if (Device.isDevice) {
+      const projectId = Constants.expoConfig?.extra ? Constants.expoConfig?.extra.eas.projectId : undefined;
+      if (projectId) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+              const { status } = await Notifications.requestPermissionsAsync();
+              finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+              alert('Failed to get push token for push notification!');
+              return;
+          }
+          // Learn more about projectId:
+          // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+          token = (await Notifications.getExpoPushTokenAsync({ projectId })).data;
+      } else {
+          alert('Must use physical device for Push Notifications');
+      }
+  }
+  return token;
+}
+
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-
 export default function RootLayout() {
-
-
   return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => console.log(token));
+    let isMounted = true;
+
+    function redirect(notification: Notifications.Notification) {
+      Notifications.dismissNotificationAsync(notification.request.identifier);
+      const url = notification.request.content.data?.url;
+      if (url) {
+        router.push(url);
+      }
+    }
+
+    Notifications.getLastNotificationResponseAsync()
+      .then(response => {
+        if (!isMounted || !response?.notification) {
+          return;
+        }
+        redirect(response?.notification);
+      });
+
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      Notifications.dismissNotificationAsync(response.notification.request.identifier);
+      console.log(response);
+      // aknowledge the notification
+
+      redirect(response.notification);
+    });
+
+    return () => {
+      isMounted = false;
+      subscription.remove();0
+    };
+  }, []);
   const colorScheme = useColorScheme();
 
   return (
