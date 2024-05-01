acb9091edb04bccdf5f51cce01a19c905b4fc0f0
exatombe <jeremy27.clara22@gmail.com>
2024-05-01 23:28:24 +0200

    Removing Data that could be a trouble, adding Constant package to retrieve data such a ProjectId to miss nothing.

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,11 +1,12 @@
 import * as Notifications from "expo-notifications";
-import  * as Device from "expo-device";
+import * as Device from "expo-device";
 import { Button, Surface, Text } from 'react-native-paper';
 import { useEffect, useRef, useState } from "react";
 import { Platform } from "react-native";
+import Constants from "expo-constants";
 
 Notifications.setNotificationHandler({
-    handleNotification: async() => ({
+    handleNotification: async () => ({
         shouldPlaySound: false,
         shouldSetBadge: true,
         shouldShowAlert: true
@@ -20,27 +21,27 @@ export default function CreateScreen() {
     const responseListener = useRef<Notifications.Subscription>();
 
     useEffect(() => {
-      registerForPushNotificationsAsync().then(token => {
-        if(token){
-            setExpoPushToken(token)
-            console.log(token);
-        }
-      });
+        registerForPushNotificationsAsync().then(token => {
+            if (token) {
+                setExpoPushToken(token)
+                console.log(token);
+            }
+        });
 
-      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-        setNotification(notification);
-      });
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
 
-      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-        console.log(response);
-      });
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
 
-      return () => {
-        if(notificationListener.current && responseListener.current){
-           Notifications.removeNotificationSubscription(notificationListener.current);
-            Notifications.removeNotificationSubscription(responseListener.current);
-        }
-      };
+        return () => {
+            if (notificationListener.current && responseListener.current) {
+                Notifications.removeNotificationSubscription(notificationListener.current);
+                Notifications.removeNotificationSubscription(responseListener.current);
+            }
+        };
     }, []);
 
 
@@ -49,19 +50,19 @@ export default function CreateScreen() {
             flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
-          }}>
+        }}>
             <Text style={{ textAlign: 'center' }} variant='titleLarge'>Create a new app</Text>
             <Text>Your expo push token: {expoPushToken}</Text>
-      <Surface style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-      </Surface>
-      <Button
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      >Press a Schedule notification</Button>
+            <Surface style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Title: {notification && notification.request.content.title} </Text>
+                <Text>Body: {notification && notification.request.content.body}</Text>
+                <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+            </Surface>
+            <Button
+                onPress={async () => {
+                    await schedulePushNotification();
+                }}
+            >Press a Schedule notification</Button>
         </Surface>
     );
 }
@@ -70,45 +71,47 @@ export default function CreateScreen() {
 
 async function schedulePushNotification() {
     await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "You've got mail! 📬",
-        body: 'Here is the notification body',
-        data: { data: 'goes here' },
-      },
-      trigger: { seconds: 2 },
+        content: {
+            title: "You've got mail! 📬",
+            body: 'Here is the notification body',
+            data: { data: 'goes here' },
+        },
+        trigger: { seconds: 2 },
     });
-  }
+}
 
-  async function registerForPushNotificationsAsync() {
+async function registerForPushNotificationsAsync() {
     let token;
 
     if (Platform.OS === 'android') {
-      await Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
     }
 
     if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      // Learn more about projectId:
-      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
-      token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
-      console.log(token);
-    } else {
-      alert('Must use physical device for Push Notifications');
+        const projectId = Constants.expoConfig?.extra ? Constants.expoConfig?.extra.eas.projectId : undefined;
+        if (!projectId) {
+            const { status: existingStatus } = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            // Learn more about projectId:
+            // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+            token = (await Notifications.getExpoPushTokenAsync({ projectId })).data;
+            console.log(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
     }
-
     return token;
-  }
+}
